ce46facf702f29f7a1b315f73cd7913a13fa2faa
Faith Loredo <FaithLoredo@10.48.25.202>
2020-03-30 23:51:24 -0400

    Created initial work for job searching

--- RK_LAB1/Models/User.cs
+++ RK_LAB1/Models/User.cs
@@ -8,11 +8,14 @@ namespace RK_LAB1.Models
 {
     public class User
     {
+        [Key]
         [Required (ErrorMessage ="Please Enter your Username")]
         public string Username { get; set; }
 
         [Required(ErrorMessage ="Please Enter your password")]
         protected string Password { get; set; }
+        [Required(ErrorMessage ="Please Enter your email")]
+        public string Email { get; set; }
 
     }
 }
